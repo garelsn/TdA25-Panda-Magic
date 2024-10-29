01ed8653ea8149b646c723a5f3fd0a8eb671880c
garelsn <kkitr37@gmail.com>
2024-10-29 12:30:19 +0100

    Update app.py
    Hello Tda

--- app/app.py
+++ app/app.py
@@ -20,7 +20,7 @@ db.init_app(app)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return "Hello Tour de App!"
+    return "Hello TdA 🐼 Magic "
 
 @app.route('/api', methods=['GET'])
 def api(): 

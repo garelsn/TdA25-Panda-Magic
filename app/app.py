7459874c79a6520f1156ccf0a35168acc1947ced
Onhalar <halaczondrej@gmail.com>
2024-11-08 16:44:27 +0100

    tried to make the post request

--- app/app.py
+++ app/app.py
@@ -17,6 +17,38 @@ except OSError:
 
 db.init_app(app)
 
+# global variables ---------------------
+
+uuid: int
+board = []
+
+DBValues = {
+    ' ': None,
+    'X': True,
+    'O': False
+}
+LocalValues = { DBValues[key]:key for key in DBValues }
+
+def parseBoardOut(board: list):
+    return [ [ LocalValues[y] for y in x ] for x in board]
+
+def parseBoardIn(board: list):
+    return [ [ DBValues[y] for y in x ] for x in board]
+
+def initBoard():
+    for i in range(15):
+        boardLayer = []
+        for j in range(15):
+            boardLayer.append(None)
+        board.append(boardLayer)
+
+@app.rounte('/games', methods=['POST'])
+def initNewGame():
+    board.clear() # later function that saves the board
+    initBoard()
+
+    return jsonify(parseBoardOut(board))
+
 
 @app.route('/')
 def hello_world():  # put application's code here

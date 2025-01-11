d5bd84ec694f2ab6ac4b860ac9a8d546025ddee0
Onhalar <halaczondrej@gmail.com>
2025-01-11 16:40:56 +0100

    Confettin fires upon reaching 5

--- react/src/components/WinAnimation/WinAnimation.tsx
+++ react/src/components/WinAnimation/WinAnimation.tsx
@@ -1,10 +1,12 @@
 import Confetti from "react-confetti";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Board from "../Game/Components/Board";
 type BoardProps = {
   board: string[][];
 };
 
 const WinAnimation: React.FC<BoardProps> = ({ board }) => {
+
   // Define the type for windowSize
   const [isConfettiActive, setIsConfettiActive] = useState(false);
 
@@ -21,31 +23,64 @@ const WinAnimation: React.FC<BoardProps> = ({ board }) => {
     });
   }
 
+  useEffect(() => { if (!isConfettiActive) {window.onresize = null; }}, [isConfettiActive]) // clearing the onresize event listener
+
   function fireConfetti() {
     // set event handler
     window.onresize = () => { handleWindowSizeChange(); }
-
+    
     setIsConfettiActive(true);
+  }
 
-    setTimeout(() => {
-      setIsConfettiActive(false); 
-    }, 6500);
+  function searchForWin() {
+    if (isConfettiActive) {
+      return;
+    }
 
-    // remove event handler (setTimout shold be async)
-    setTimeout(() => {
-      window.onresize = null;  // Remove the resize listener after confetti animation ends for performance reasons of course
-    }, 6500);
+    // search patterns
+    const searchPatterns: Map<string, number[][]> = new Map<string, number[][]>([
+      ['horizontalSet', [[-2, 0], [-1, 0], [1, 0], [2, 0]]],
+      ['verticalSet', [[0, -2], [0, -1], [0, 1], [0, 2]]],
+      ['diagonalSet', [[-2, -2], [-1, -1], [1, 1], [2, 2]]],
+      ['antiDiagonalSet', [[2, -2], [1, -1], [-1, 1], [-2, 2]]]
+    ])
+    function searchByPattern(pattern: number[][], position: number[]): Boolean {
+      var symbolToMatch = board[position[0]][position[1]];
+
+      if (symbolToMatch == '') {
+        return false;
+      }
+      for (var modifier of pattern) {
+        try {
+          if (board[position[0] + modifier[0]][position[1] + modifier[1]] != symbolToMatch) {
+            return false;
+          }
+        } catch (e) {
+          return false;
+        }
+      }
+
+      return true;
+    }
+    for (let y = 0; y < board.length; y++) {
+      for (let x = 0; x < board[y].length; x++) {
+        for (let searchPatternKey of searchPatterns) {
+          if (searchByPattern(searchPatternKey[1] as number[][], [y, x])) {
+            fireConfetti();
+            return;
+          }
+        }
+      }
+    }
+  }
+
+  if (board != null) {
+    searchForWin();
   }
 
   return (
     <div className="">
       {isConfettiActive && <Confetti width={windowSize.width} height={windowSize.height} />}
-      <button
-        onClick={fireConfetti}
-        className="bg-white w-24 h-12"
-      >
-        Výhra
-      </button>
     </div>
   );
 }

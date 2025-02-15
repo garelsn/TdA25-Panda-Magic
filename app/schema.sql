370231123b13d830302abf5f684919afd9e164b2
garelsn <kkitr37@gmail.com>
2025-02-15 11:07:38 +0100

    add table sqlite users
    add table sqlite users

--- app/schema.sql
+++ app/schema.sql
@@ -13,3 +13,20 @@ CREATE TABLE IF NOT EXISTS games (
     createdAt TEXT NOT NULL,
     updatedAt TEXT NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS users (
+    uuid TEXT PRIMARY KEY,
+    createdAt TEXT NOT NULL,
+    loginAt TEXT NOT NULL,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    elo REAL NOT NULL DEFAULT 0.0,
+    countGames INTEGER NOT NULL DEFAULT 0,
+    wins INTEGER NOT NULL DEFAULT 0,
+    draws INTEGER NOT NULL DEFAULT 0,
+    losses INTEGER NOT NULL DEFAULT 0,
+    profilImage TEXT NOT NULL DEFAULT 'default.webp',
+    ban BOOLEAN NOT NULL DEFAULT FALSE,
+    admin BOOLEAN NOT NULL DEFAULT FALSE
+);

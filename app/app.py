e5913188a7b225c05dd656e3ca62e85f5364180f
Onhalar <halaczondrej@gmail.com>
2025-02-16 15:47:29 +0100

    Delete user by ID

--- app/app.py
+++ app/app.py
@@ -345,6 +345,34 @@ def returnUserById(uuid):
 
     return jsonify(result), 200
 
+@app.route("/api/v1/users/<uuid>", methods=["DELETE"])
+def deleteUserById(uuid):
+    sqlDB = db.get_db()
+    cursor = sqlDB.cursor()
+    
+    # Najdi položku podle UUID
+    cursor.execute("SELECT * FROM users WHERE uuid=?", (uuid,))
+    DBItem = cursor.fetchone()
+    
+    if DBItem is None:
+        return jsonify(
+            {
+                "code": 404,
+                "message": "Resource not found"
+            }
+        ), 404
+
+    cursor.execute("DELETE FROM users WHERE uuid=?", (uuid,))
+    sqlDB.commit()
+    sqlDB.close()
+
+    return jsonify(
+        {
+            "code": 204,
+            "message": f"Game with UUID {uuid} successfully deleted"
+        }
+    ), 204
+
 if __name__ == '__main__':
     app.run(debug=True)
 
